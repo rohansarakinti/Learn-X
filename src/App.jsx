2bd0fab590bb4e8bbd23c257fac531ad676c2b65
rohansarakinti <sarakintirohan@gmail.com>
2023-11-25 10:45:17 -0600

    fixed NextUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import {Button} from "@nextui-org/react";
+
 export default function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button color='primary'>
+      Button
+    </Button>
+    </>
   )
 }
