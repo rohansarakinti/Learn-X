2c7e524d4f12dfe21f21f369d510a88ae72cf763
PanavMhatre <mhatrepanav@gmail.com>
2023-12-10 11:29:26 -0600

    Created course icon card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { createBrowserRouter, RouterProvider }  from "react-router-dom";
-import { NextUIProvider } from "@nextui-org/react";
-import Home from "./pages/Home";
+// import { createBrowserRouter, RouterProvider }  from "react-router-dom";
+// import { NextUIProvider } from "@nextui-org/react";
+// import Home from "./pages/Home";
 
-export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-    },
-  ])
-  return(
-    <NextUIProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </NextUIProvider>
-  )
+// export default function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: '/',
+//       element: <Home />,
+//     },
+//   ])
+//   return(
+//     <NextUIProvider>
+//       <RouterProvider router={router}></RouterProvider>
+//     </NextUIProvider>
+//   )
+// }
+import CourseIcon from "./components/CourseIcon";
+function App() {
+  return (
+    <div>
+      <CourseIcon></CourseIcon>
+    </div>
+  );
 }
+
+export default App;
