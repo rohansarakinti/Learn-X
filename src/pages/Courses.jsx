8324186778eff52d75570f9fac3307fc72c441f0
PanavMhatre <mhatrepanav@gmail.com>
2024-01-13 13:59:05 -0600

    Course

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,5 +1,14 @@
+import Sidebar from "../components/Sidebar";
+
 function Courses() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar />
+      <div className="p-4 sm:ml-64">
+        <h1 className="text-black">Hello</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Courses;
