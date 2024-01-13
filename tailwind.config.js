9fa96e8a2188136e148a742b4cd6ade50cfc8a68
hawkings232 <asiankidjoe6@gmail.com>
2024-01-13 17:01:43 -0600

    Delete unused assets and update file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ import { nextui } from "@nextui-org/react"
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {
