1aad59c78149de463e660b488e82d9dc2b838e58
PanavMhatre <mhatrepanav@gmail.com>
2024-01-13 11:16:10 -0600

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
 import SignUpPage from "./pages/SignUpPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
-import HeroPage from "./pages/HeroPage.jsx"
+import HeroPage from "./pages/HeroPage.jsx";
+import CoursePage from "./pages/CoursePage.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HeroPage />}/>
-      <Route path="/home" element={<LandingPage />}/>
+      <Route path="/" element={<HeroPage />} />
+      <Route path="/home" element={<LandingPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/coursePage" element={<CoursePage></CoursePage>}></Route>
     </Routes>
   );
 }
