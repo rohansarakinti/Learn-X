ff6267c5ad2bae1d11c5cd7fef3c135dc18235ea
PanavMhatre <mhatrepanav@gmail.com>
2024-01-15 11:29:11 -0600

    Teacher Section

--- client/src/pages/TeacherCoursePage.jsx
+++ client/src/pages/TeacherCoursePage.jsx
@@ -26,6 +26,10 @@ function TeacherCoursePage() {
               required=""
             />
           </div>
+          <p className="my-4 text-lg text-gray-500">
+            Upload content below, should be in video format in mp4 format.
+            Thanks for creating a course on LearnX
+          </p>
           <div>
             <label
               htmlFor="email"
@@ -41,6 +45,43 @@ function TeacherCoursePage() {
               placeholder="X2Y3Z4"
               required=""
             />
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  PDF, PNG, JPG or GIF (MAX. 800x400px)
+                </p>
+              </div>
+              <input id="dropzone-file" type="file" className="hidden" />
+            </label>
           </div>
           <div className="modal-action">
             <form method="dialog">
