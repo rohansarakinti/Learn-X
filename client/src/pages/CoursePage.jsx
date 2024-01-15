47fab36eb7f7d2463992f991406057caa6ffc834
PanavMhatre <mhatrepanav@gmail.com>
2024-01-15 13:39:50 -0600

    coming soon

--- client/src/pages/CoursePage.jsx
+++ client/src/pages/CoursePage.jsx
@@ -64,50 +64,20 @@ function CoursePage() {
         ))}
       </div>
       <button
-        type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 justify-center focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        onClick={() => "my_modal_1".showModal()}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
       >
         Add More Courses
       </button>
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">SignUp for more courses</h3>
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Class Name
-            </label>
-            <input
-              type="shorttext"
-              name="shorttext"
-              id="shorttext"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Accounting"
-              required=""
-            />
-          </div>
-          <p className="py-4">Or enter the class code</p>
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Class Code
-            </label>
-            <input
-              type="shorttext"
-              name="shorttext"
-              id="shorttext"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="X2Y3Z4"
-              required=""
-            />
-          </div>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
           <div className="modal-action">
             <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
               <button className="btn">Close</button>
             </form>
           </div>
