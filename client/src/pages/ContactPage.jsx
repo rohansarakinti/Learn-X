035e9ef9be76c524a2dc03acabe3893eb81c3701
rohansarakinti <sarakintirohan@gmail.com>
2024-01-15 11:58:05 -0600

    asset fix

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,5 +1,6 @@
 import { Input, Button } from "@nextui-org/react"
 import Navbar from "../components/Navbar"
+//import Account from "../assets/Account.png"
 
 
 export default function ContactPage() {
